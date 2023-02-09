509d4522b6305656af16bda9d73ad32eb9bf5dd9
Thienluc <nguyenthienluc2004@gmail.com>
2023-02-09 12:07:03 +0700

    update

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5500",
-  // baseURL: "https://locofy-auth.onrender.com",
+  // baseURL: "http://localhost:5500",
+  baseURL: "https://locofy-auth.onrender.com",
   headers: {
     "Content-Type": "application/json",
     "Cache-Control": "no-cache",
